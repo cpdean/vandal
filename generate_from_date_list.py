d58cc67d038028747c77bbc367198b06c3242e31
Conrad Dean <conrad.p.dean@gmail.com>
2013-01-19 18:03:40 -0600

    change generate example to work off JS data

--- generate_from_date_list.py
+++ generate_from_date_list.py
@@ -3,6 +3,7 @@ import time
 import os
 import datetime
 import subprocess
+import dateutil.parser
 
 
 def get_datetime_from_commit(c):
@@ -28,15 +29,29 @@ def new_date():
     return datetime.datetime.utcnow()
 
 
-def days_from(n):
-    d = {"year": 2012, "day": 1, "month": 4}
-    date = new_date().replace(**d)
+def days_from(n, date=None):
+    if date is None:
+        d = {"year": 2012, "day": 1, "month": 4}
+        date = new_date().replace(**d)
     return date + datetime.timedelta(days=n)
 
 
 def add_commit_to(r, n):
     directory = r.working_dir
     date = days_from(n)
+    args = ["git", "commit", "-m", "heybro", "--allow-empty"]
+    env = {}
+    env["GIT_AUTHOR_DATE"] = date.isoformat()
+    env["GIT_COMMITTER_DATE"] = date.isoformat()
+    process = subprocess.Popen(args,
+                               cwd=directory,
+                               env=env,
+                               stdout=subprocess.PIPE)
+    return process.wait()
+
+
+def add_commit_with_date(r, date):
+    directory = r.working_dir
     args = ["git", "commit", "-m", "heybro", "--allow-empty"]
     env = {}
     env["GIT_AUTHOR_DATE"] = date.isoformat()
@@ -55,8 +70,16 @@ def print_dates(r):
         print get_hexsha(c), get_datetime_from_commit(c), c.message.strip()
 
 
+def generate(r, iso_date, painted):
+    date = dateutil.parser.parse(iso_date)
+    for i in range(len(painted)):
+        if painted[i] == 1:
+            current_date = days_from(i, date)
+            add_commit_with_date(r, current_date)
+
+
 if __name__ == '__main__':
     repo = test_repo()
-    #for i in range(300*4):
-    for i in range(10):
-        add_commit_to(repo, i)
+    date = "2012-01-22T06:00:00.000Z"
+    painted = [0, 1, 0, 1, 0, 1, 0]
+    generate(repo, date, painted)
